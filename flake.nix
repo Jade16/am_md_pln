a4e569febf7bed0b31422da96aab913ee73b4385
lucasgpulcinelli <lucasegp@usp.br>
2024-11-19 14:18:59 -0300

    add initial exploratory analysis code + un training

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "Diego";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    ...
+  } @ inputs:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = inputs.nixpkgs.legacyPackages.${system};
+    in {
+      devShells.default = pkgs.mkShell {
+        packages = with pkgs; [
+          (pkgs.python311.withPackages (python-pkgs: [
+            python-pkgs.streamlit
+            python-pkgs.seaborn
+            python-pkgs.numpy
+            python-pkgs.matplotlib
+            python-pkgs.pandas
+            python-pkgs.transformers
+            python-pkgs.datasets
+            python-pkgs.seqeval
+            python-pkgs.torch
+            python-pkgs.wandb
+            python-pkgs.wordcloud
+            python-pkgs.accelerate
+          ]))
+        ];
+      };
+    });
+}
+
+
+
